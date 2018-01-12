333d27d6a8c4af8c4d5469dda908a431b9e820a8
Neamar <neamar@neamar.fr>
2018-01-12 17:08:34 +0000

    Add data as js

---
+++ fake_html_scaffold/data.js
@@ -0,0 +1,114 @@
+var ritoverlayItems = [
+  {
+    "item": {
+      "description": "<groupLimit>Limited to 1 pair of boots.</groupLimit><br><br><br><br><unique>UNIQUE Passive - Enhanced Movement:</unique> +45 Movement Speed",
+      "image_path": "/home/neamar/src/twitch-overlay/get_items/items/3020.png",
+      "image_url": "http://ddragon.leagueoflegends.com/cdn/8.1.1/img/item/3020.png",
+      "id": "3020",
+      "stats": "<li>+18 <a href='FlatMagicPen'>Magic Penetration</a></li>",
+      "name": "Sorcerer's Shoes"
+    },
+    "position": {
+      "x": 1163,
+      "y": 956,
+      "w": 39,
+      "h": 39
+    }
+  },
+  {
+    "item": {
+      "description": "<groupLimit>Limited to 1 pair of boots.</groupLimit><br><br><br><br><unique>UNIQUE Passive - Enhanced Movement:</unique> +45 Movement Speed",
+      "image_path": "/home/neamar/src/twitch-overlay/get_items/items/3020.png",
+      "image_url": "http://ddragon.leagueoflegends.com/cdn/8.1.1/img/item/3020.png",
+      "id": "3020",
+      "stats": "<li>+18 <a href='FlatMagicPen'>Magic Penetration</a></li>",
+      "name": "Sorcerer's Shoes"
+    },
+    "position": {
+      "x": 851,
+      "y": 331,
+      "w": 34,
+      "h": 34
+    }
+  },
+  {
+    "item": {
+      "description": "<groupLimit>Limited to 1 pair of boots.</groupLimit><br><br><br><br><unique>UNIQUE Passive - Enhanced Movement:</unique> +45 Movement Speed",
+      "image_path": "/home/neamar/src/twitch-overlay/get_items/items/3020.png",
+      "image_url": "http://ddragon.leagueoflegends.com/cdn/8.1.1/img/item/3020.png",
+      "id": "3020",
+      "stats": "<li>+18 <a href='FlatMagicPen'>Magic Penetration</a></li>",
+      "name": "Sorcerer's Shoes"
+    },
+    "position": {
+      "x": 1388,
+      "y": 407,
+      "w": 34,
+      "h": 34
+    }
+  },
+  {
+    "item": {
+      "description": "<groupLimit>Limited to 1 pair of boots.</groupLimit><br><br><br><br><unique>UNIQUE Passive - Enhanced Movement:</unique> +45 Movement Speed",
+      "image_path": "/home/neamar/src/twitch-overlay/get_items/items/3020.png",
+      "image_url": "http://ddragon.leagueoflegends.com/cdn/8.1.1/img/item/3020.png",
+      "id": "3020",
+      "stats": "<li>+18 <a href='FlatMagicPen'>Magic Penetration</a></li>",
+      "name": "Sorcerer's Shoes"
+    },
+    "position": {
+      "x": 1388,
+      "y": 635,
+      "w": 34,
+      "h": 34
+    }
+  },
+  {
+    "item": {
+      "description": "<br><br><unique>UNIQUE Passive - Eyes of Pain:</unique> +15 <a href='FlatMagicPen'>Magic Penetration</a><br><unique>UNIQUE Passive:</unique> Spells burn enemies for 3 seconds, dealing bonus magic damage equal to 2% of their current Health per second. Burn damage is doubled against <a href='MovementImpaired'>movement-impaired</a> units.",
+      "image_path": "/home/neamar/src/twitch-overlay/get_items/items/3151.png",
+      "image_url": "http://ddragon.leagueoflegends.com/cdn/8.1.1/img/item/3151.png",
+      "id": "3151",
+      "stats": "<li>+80 Ability Power</li>\n<li>+300 Health</li>",
+      "name": "Liandry's Torment"
+    },
+    "position": {
+      "x": 716,
+      "y": 332,
+      "w": 31,
+      "h": 31
+    }
+  },
+  {
+    "item": {
+      "description": "<br><br><unique>UNIQUE Passive - Eyes of Pain:</unique> +15 <a href='FlatMagicPen'>Magic Penetration</a><br><unique>UNIQUE Passive:</unique> Spells burn enemies for 3 seconds, dealing bonus magic damage equal to 2% of their current Health per second. Burn damage is doubled against <a href='MovementImpaired'>movement-impaired</a> units.",
+      "image_path": "/home/neamar/src/twitch-overlay/get_items/items/3151.png",
+      "image_url": "http://ddragon.leagueoflegends.com/cdn/8.1.1/img/item/3151.png",
+      "id": "3151",
+      "stats": "<li>+80 Ability Power</li>\n<li>+300 Health</li>",
+      "name": "Liandry's Torment"
+    },
+    "position": {
+      "x": 716,
+      "y": 484,
+      "w": 31,
+      "h": 31
+    }
+  },
+  {
+    "item": {
+      "description": "<br><br><unique>UNIQUE Passive - Eyes of Pain:</unique> +15 <a href='FlatMagicPen'>Magic Penetration</a><br><unique>UNIQUE Passive:</unique> Spells burn enemies for 3 seconds, dealing bonus magic damage equal to 2% of their current Health per second. Burn damage is doubled against <a href='MovementImpaired'>movement-impaired</a> units.",
+      "image_path": "/home/neamar/src/twitch-overlay/get_items/items/3151.png",
+      "image_url": "http://ddragon.leagueoflegends.com/cdn/8.1.1/img/item/3151.png",
+      "id": "3151",
+      "stats": "<li>+80 Ability Power</li>\n<li>+300 Health</li>",
+      "name": "Liandry's Torment"
+    },
+    "position": {
+      "x": 1287,
+      "y": 484,
+      "w": 31,
+      "h": 31
+    }
+  }
+]
