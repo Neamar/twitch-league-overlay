672e0cc3bcf1c10b5b6f288c9cf3816870074155
Neamar <neamar@neamar.fr>
2018-01-12 16:29:04 +0000

    Improved logging and performance

--- parse_screenshot/parse_screenshot.py
+++ parse_screenshot/parse_screenshot.py
@@ -10,11 +10,12 @@ def find_item_in_screenshot(screenshot_cv2_data, item):
     Returns an array of dictionary(x, y, w, h, value)
     """
     points = []
-    template = cv2.imread(item['image_path'], 0)
+    template = cv2.imread(item['image_path'])
+    template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 
     w, h = template.shape[::-1]
 
-    for scale in np.linspace(0.2, 1.0, 20)[::-1]:
+    for scale in np.linspace(0.4, 0.8, 10)[::-1]:
         # resize the image according to the scale, and keep track
         # of the ratio of the resizing
         new_width = int(template.shape[1] * scale)
@@ -35,14 +36,16 @@ def find_item_in_screenshot(screenshot_cv2_data, item):
                     already_exists = True
                     break
             if not already_exists:
-                print("New match for %s at (%s, %s)" % (item['name'], potential_point[0], potential_point[1]))
+                value = res[potential_point[1]][potential_point[0]]
                 points.append({
                     "x": potential_point[0],
                     "y": potential_point[1],
                     "w": w * r,
                     "h": h * r,
-                    "value": res[potential_point[1]][potential_point[0]]
+                    "value": value,
                 })
+    for point in points:
+        print("Found %s at (%s, %s, %s, %s), value %s" % (item['name'], point['x'], point['y'], point['w'], point['h'], point['value']))
     return points
 
 
@@ -50,11 +53,13 @@ def parse_screenshot(screenshot_path, items):
     img_rgb = cv2.imread(screenshot_path)
     original = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
+    items_position = []
     for item in items:
         print(" Looking for %s" % item['name'])
 
         # Dictionary of x, y, w, h, value
         points = find_item_in_screenshot(original, item)
+        items_position.append(points)
 
         for pt in points:
             cv2.rectangle(img_rgb, (pt['x'], pt['y']), (pt['x'] + int(pt['w']), pt['y'] + int(pt['h'])), (0, 0, 255), 2)
