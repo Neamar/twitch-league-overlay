3ac9dc04742f4af13b3136672a0832edc459ec77
Neamar <neamar@neamar.fr>
2018-01-12 12:41:17 +0000

    Make parse_screenshot a function

--- parse_screenshot/parse_screenshot.py
+++ parse_screenshot/parse_screenshot.py
@@ -1,16 +1,29 @@
 import cv2
 import numpy as np
+import json
 
-img_rgb = cv2.imread('stream1.png')
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-template = cv2.imread('liandry.png', 0)
-print(template.shape)
-w, h = template.shape[::-1]
 
-res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-threshold = 0.6
-loc = np.where(res >= threshold)
-for pt in zip(*loc[::-1]):
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+def parse_screenshot(screenshot_path, items):
+    img_rgb = cv2.imread(screenshot_path)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
-cv2.imwrite('res.png', img_rgb)
+    for item in items:
+        print("Looking for %s" % item['name'])
+
+        template = cv2.imread(item['image_path'], 0)
+        w, h = template.shape[::-1]
+
+        res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+        threshold = 0.6
+        loc = np.where(res >= threshold)
+        for pt in zip(*loc[::-1]):
+            cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+
+    cv2.imwrite('res.png', img_rgb)
+
+if __name__ == "__main__":
+    screenshot_path = "stream1.png"
+    with open('../get_items/items_simplified.json') as f:
+        items = json.loads(f.read())
+
+    parse_screenshot(screenshot_path, items)
