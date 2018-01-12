e3bde5e9eb0c668c0f3706ce6e93f11c4568db00
Neamar <neamar@neamar.fr>
2018-01-12 12:11:01 +0000

    Download items in items/ folder

--- get_items/get_items.py
+++ get_items/get_items.py
@@ -12,12 +12,23 @@ def get_items():
         result = r.json()
         item_data = result['data']
         for item_id, item_data in item_data.items():
+            image_url = image_template % item_data['image']['full']
+            # Download image
+            path = 'items/%s.png' % item_id
+            img = requests.get(image_url, stream=True)
+            if img.status_code == 200:
+                with open(path, 'wb') as f:
+                    for chunk in img:
+                        f.write(chunk)
+
             items.append({
                  "id": item_id,
                  "name": item_data['name'],
                  "description": 'TBD',  # item_data['description'],
                  "image_url": image_template % item_data['image']['full']
               })
+
+
     return items
 
 if __name__ == "__main__":
