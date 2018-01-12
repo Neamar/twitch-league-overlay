6cb6ef906978b6816c83f001af1c84c0647129e6
Neamar <neamar@neamar.fr>
2018-01-12 11:49:16 +0000

    Read from League data

--- parse_screenshot/parse_screenshot.py
+++ parse_screenshot/parse_screenshot.py
@@ -1,13 +1,14 @@
 import cv2
 import numpy as np
 
-img_rgb = cv2.imread('mario.png')
+img_rgb = cv2.imread('stream1.png')
 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-template = cv2.imread('coin.png', 0)
+template = cv2.imread('liandry.png', 0)
+print(template.shape)
 w, h = template.shape[::-1]
 
 res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-threshold = 0.8
+threshold = 0.6
 loc = np.where(res >= threshold)
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
