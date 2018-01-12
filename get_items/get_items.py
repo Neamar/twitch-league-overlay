bcb36fe526e78851ed4c21990256968c51ce7962
Neamar <neamar@neamar.fr>
2018-01-12 17:00:40 +0000

    Generate better items description

--- get_items/get_items.py
+++ get_items/get_items.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+import re
 
 PATCH = "8.1.1"
 image_template = "http://ddragon.leagueoflegends.com/cdn/8.1.1/img/item/%s"
@@ -15,21 +16,31 @@ def get_items():
         item_data = result['data']
         for item_id, item_data in item_data.items():
             image_url = image_template % item_data['image']['full']
-            # Download image
-            path = 'items/%s.png' % item_id
-            img = requests.get(image_url, stream=True)
-            if img.status_code == 200:
-                with open(path, 'wb') as f:
-                    for chunk in img:
-                        f.write(chunk)
+            # # Download image
+            # path = 'items/%s.png' % item_id
+            # img = requests.get(image_url, stream=True)
+            # if img.status_code == 200:
+            #     with open(path, 'wb') as f:
+            #         for chunk in img:
+            #             f.write(chunk)
+
+            stats = re.search('<stats>(.+)</stats>', item_data['description'])
+            if(stats):
+                stats = stats.group(1)
+                stats = stats.split('<br>')
+                stats = map(lambda s: '<li>%s</li>' % s, stats)
+                stats = "\n".join(stats)
+
+            description = re.sub('<stats>(.+)</stats>', '', item_data['description'])
 
             items.append({
                  "id": item_id,
                  "name": item_data['name'],
-                 "description": 'TBD',  # item_data['description'],
+                 "stats": stats if stats else "",
+                 "description": description,
                  "image_url": image_url,
                  "image_path": os.path.abspath('items/%s.png' % item_id)
-              })
+            })
 
     return items
 
