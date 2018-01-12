6a26d23685b1b7b7cd20ba202b0c567dc17ff357
Neamar <neamar@neamar.fr>
2018-01-12 12:47:36 +0000

    Run algorithm on multiple resolution of item

--- parse_screenshot/parse_screenshot.py
+++ parse_screenshot/parse_screenshot.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import imutils
 import json
 
 
@@ -13,11 +14,19 @@ def parse_screenshot(screenshot_path, items):
         template = cv2.imread(item['image_path'], 0)
         w, h = template.shape[::-1]
 
-        res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-        threshold = 0.6
-        loc = np.where(res >= threshold)
-        for pt in zip(*loc[::-1]):
-            cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+        for scale in np.linspace(0.2, 1.0, 20)[::-1]:
+            # resize the image according to the scale, and keep track
+            # of the ratio of the resizing
+            new_width = int(template.shape[1] * scale)
+            resized = imutils.resize(template, width=new_width)
+            r = float(resized.shape[1]) / float(template.shape[1])
+
+            res = cv2.matchTemplate(img_gray, resized, cv2.TM_CCOEFF_NORMED)
+            threshold = 0.7
+            loc = np.where(res >= threshold)
+            for pt in zip(*loc[::-1]):
+                print("Got a match for %s" % (item['name']))
+                cv2.rectangle(img_rgb, pt, (pt[0] + int(w * r), pt[1] + int(h * r)), (0, 0, 255), 2)
 
     cv2.imwrite('res.png', img_rgb)
 
